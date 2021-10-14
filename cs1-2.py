0c18a539d2393800097fb1f8fb8e5d57e3415924
p22cgussen <p22cgussen@fairfieldprep.org>
2021-10-14 10:10:56 -0400

    edit cs1-2

--- cs1-2.py
+++ cs1-2.py
@@ -4,6 +4,6 @@ amount = input("What is the investment amount?")
 rate = input("What is the annual intrest rate as a percent?")
 years = input("How many years is the money in the account?")
 
-value = int(amount) * (1 + float(rate) / 12) ** float(years)
+value = int(amount) * (1 + float(rate) / 12) ** (float(years) * 12)
 
 print("Your account now has " + str(value) + " USD")
